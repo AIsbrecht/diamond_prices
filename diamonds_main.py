0743c74b7e47ae2f8b3bcaf330d84740ced43b17
AIsbrecht <isbrechtalex@gmail.com>
2025-01-12 17:21:09 -0500

    Import Statements
    Got the basic imports in place and started getting the path to the csv set up.

---
+++ diamonds_main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+df = pd.read_csv("C:\Users\isbre\OneDrive\Documents\GitHub\diamond_prices\data\diamonds.csv")
+print(df.head())
