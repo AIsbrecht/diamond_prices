2ff9113613dbaea5e49ef72fdea47e498d83a692
AIsbrecht <isbrechtalex@gmail.com>
2025-01-13 13:52:42 -0500

    Switched diamonds_main over to a notebook

--- diamonds_main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-
-df = pd.read_csv("C:\\Users\\isbre\\OneDrive\\Documents\\GitHub\\diamond_prices\\data\\diamonds.csv")
-print(df.isna().sum())
