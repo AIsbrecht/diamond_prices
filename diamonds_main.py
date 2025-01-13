15f105fe0c41bb778aec0a4105c317cc1f04c18b
AIsbrecht <isbrechtalex@gmail.com>
2025-01-12 22:33:06 -0500

    got data import statement work

--- diamonds_main.py
+++ diamonds_main.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-df = pd.read_csv("C:\Users\isbre\OneDrive\Documents\GitHub\diamond_prices\data\diamonds.csv")
-print(df.head())
+df = pd.read_csv("C:\\Users\\isbre\\OneDrive\\Documents\\GitHub\\diamond_prices\\data\\diamonds.csv")
+print(df.isna().sum())
